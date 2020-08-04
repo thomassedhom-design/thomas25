381d8caca5b291931f10ea3802936ac1e1fb5014
thomassedhom-design <69202180+thomassedhom-design@users.noreply.github.com>
2020-08-04 18:18:01 +0200

    Add files via upload

---
+++ sas.js
@@ -0,0 +1,16 @@
+function month(){
+"use strict" ;
+var amount=document.getElementById("age").value,
+    result= amount * 12,
+    message=document.getElementById("message");
+    message.innerHTML= result}
+
+
+    function week(){
+      "use strict" ;
+      var mount=document.getElementById("ag").value,
+          result= mount * 48,
+          messag=document.getElementById("messag");
+          messag.innerHTML= result}
+
+
